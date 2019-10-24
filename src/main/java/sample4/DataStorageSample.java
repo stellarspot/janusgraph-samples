615f4a1dfbc6d624eb7c4b71826309edee87422a
Alexander Scherbatiy <stellarspot@gmail.com>
2019-10-24 18:44:11 +0300

    Add links to JanusGraphStorage

--- src/main/java/sample4/DataStorageSample.java
+++ src/main/java/sample4/DataStorageSample.java
@@ -1,17 +1,23 @@
 package sample4;
 
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.structure.T;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
+import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 import org.janusgraph.core.JanusGraphTransaction;
 import org.janusgraph.graphdb.database.StandardJanusGraph;
 import org.janusgraph.graphdb.idmanagement.IDManager;
 
-import java.io.Closeable;
-import java.io.IOException;
+import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.*;
 
@@ -25,6 +31,13 @@ public class DataStorageSample {
             RawNode rawNode = new RawNode("Node", "value");
             Node node = tx.getNode(rawNode);
             System.out.printf("node: %s%n", node);
+
+            RawLink rawLink = new RawLink("Link",
+                    new RawNode("Node1", "value1"),
+                    new RawNode("Node2", "value2"));
+
+            Link link = tx.getLink(rawLink);
+            System.out.printf("link: %s%n", link);
 
             tx.commit();
         }
@@ -69,7 +82,7 @@ public class DataStorageSample {
     static class RawLink extends RawAtom {
         final RawAtom[] atoms;
 
-        public RawLink(String type, RawAtom[] atoms) {
+        public RawLink(String type, RawAtom... atoms) {
             super(type);
             this.atoms = atoms;
         }
@@ -109,7 +122,7 @@ public class DataStorageSample {
 
         final long[] ids;
 
-        public Link(long id, String type, long[] ids) {
+        public Link(long id, String type, long... ids) {
             super(id, type);
             this.ids = ids;
         }
@@ -120,6 +133,11 @@ public class DataStorageSample {
 
         public Atom getAtom(JanusGraphStorageTransaction tx, int index) {
             throw new UnsupportedOperationException("Not implemented yet!");
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s(%s) - Link[%d]", type, Arrays.toString(ids), id);
         }
     }
 
@@ -180,6 +198,16 @@ public class DataStorageSample {
             return new Node(id(v), node.type, node.value);
         }
 
+        public Link getLink(RawLink link) {
+
+            Vertex v = g
+                    .inject("nothing")
+                    .union(getOrCreateLink(link))
+                    .next();
+
+            return new Link(id(v), link.type, ids(v));
+        }
+
         public void commit() {
             tx.commit();
         }
@@ -187,6 +215,17 @@ public class DataStorageSample {
         @Override
         public void close() {
             tx.close();
+        }
+
+        private GraphTraversal<Object, Vertex> getOrCreateAtom(RawAtom atom) {
+            if (atom instanceof RawNode) {
+                return getOrCreateNode((RawNode) atom);
+            } else if (atom instanceof RawLink) {
+                return getOrCreateLink((RawLink) atom);
+            } else {
+                String msg = String.format("Unknown RawAtom class: %s", atom.getClass());
+                throw new RuntimeException(msg);
+            }
         }
 
         private GraphTraversal<Object, Vertex> getOrCreateNode(RawNode node) {
@@ -205,9 +244,79 @@ public class DataStorageSample {
                     .coalesce(unfold(), addVertex);
         }
 
+        private GraphTraversal<Object, Vertex> getOrCreateLink(RawLink link) {
+
+            Function<Traverser<Object>, Iterator<String>> mapIds = t -> {
+                ArrayList arrayList = (ArrayList) t.get();
+                long[] ids = new long[arrayList.size()];
+                for (int i = 0; i < arrayList.size(); i++) {
+                    ids[i] = (long) arrayList.get(i);
+                }
+                List<String> list = new ArrayList<>(1);
+                list.add(idsToString(ids));
+                return list.iterator();
+            };
+
+            GraphTraversal<Object, Vertex> addVertex = union(getOrCreateAtoms(link))
+                    .id()
+                    .fold()
+                    .as("ids")
+                    .addV(LABEL_LINK)
+                    .property(KIND, LABEL_LINK)
+                    .property(TYPE, link.type)
+                    .property(IDS, select("ids").flatMap(mapIds))
+                    .property(T.id, storage.getNextId());
+
+            return union(getOrCreateAtoms(link))
+                    .id()
+                    .fold()
+                    .as("ids")
+                    .V()
+                    .hasLabel(LABEL_LINK)
+                    .has(KIND, LABEL_LINK)
+                    .has(TYPE, link.type)
+                    .has(IDS, select("ids").flatMap(mapIds))
+                    .fold()
+                    .coalesce(unfold(), addVertex);
+        }
+
+        private GraphTraversal<Object, Vertex>[] getOrCreateAtoms(RawLink link) {
+            int arity = link.getArity();
+            GraphTraversal<Object, Vertex>[] addAtoms = new GraphTraversal[arity];
+
+            for (int i = 0; i < arity; i++) {
+                addAtoms[i] = getOrCreateAtom(link.atoms[i]);
+            }
+            return addAtoms;
+        }
+
+
         static long id(Vertex v) {
             return (long) v.id();
         }
+
+        static long[] ids(Vertex v) {
+            String ids = v.property(IDS).value().toString();
+            return toIds(ids);
+        }
     }
 
+    static String idsToString(long... ids) {
+        StringBuilder builder = new StringBuilder();
+        for (long id : ids) {
+            builder.append(id).append(':');
+        }
+        return builder.toString();
+    }
+
+    static long[] toIds(String str) {
+        String[] split = str.split(":");
+
+        long[] ids = new long[split.length];
+
+        for (int i = 0; i < split.length; i++) {
+            ids[i] = Long.parseLong(split[i]);
+        }
+        return ids;
+    }
 }
